26329c5f57d089bde2375d7c988364771e07180a
Marcos <marcossouzasia@gmail.com>
2025-06-17 15:46:44 -0300

    subindo arquivos atualizados

---
+++ docs/firebase-config.js
@@ -0,0 +1,11 @@
+// firebase-config.js
+  const firebaseConfig = {
+    apiKey: "AIzaSyC3lLh-3ocM9QwSWRo3qNiLSAdrkfhFJ6U",
+    authDomain: "membrosigreja-e464e.firebaseapp.com",
+    projectId: "membrosigreja-e464e",
+    storageBucket: "membrosigreja-e464e.firebasestorage.app",
+    messagingSenderId: "877393002776",
+    appId: "1:877393002776:web:e5ad3d01002f6e18945e6d"
+  };
+
+firebase.initializeApp(firebaseConfig);
